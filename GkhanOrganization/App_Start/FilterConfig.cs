8784d43c0c79039d7a424dc0374574c76110e02b
SkyLord05 <gkhan.byram@gmail.com>
2019-09-17 22:35:54 +0300

    First commit

---
+++ GkhanOrganization/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GkhanOrganization
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
